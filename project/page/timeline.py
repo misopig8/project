d8a96860bca08fe194a09f84722675d581621b0a
misopig8 <misopig8@gmail.com>
2024-11-17 12:07:57 +0900

    Add files via upload

--- project/page/timeline.py
+++ project/page/timeline.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 
 # CSV 파일을 pandas로 읽기
-bus_chart = pd.read_csv('project/page/대구광역시_시내버스 정류소별_노선별_평균배차간격_20231117.csv', encoding='utf-8')
+bus_chart = pd.read_csv('대구광역시_시내버스 정류소별_노선별_평균배차간격_20231117.csv', encoding='utf-8')
 
 # Streamlit 앱 타이틀
 st.title('대구광역시 시내버스 정류장 정보 검색')
@@ -16,8 +16,15 @@ if station_name:
 
     # 정류장 이름이 있는 경우
     if not filtered_df.empty:
-        st.write(f"### {station_name}에 대한 정보:")
-        for index, row in filtered_df.iterrows():
+        # 정류장 목록을 선택할 수 있는 selectbox 제공
+        station_list = filtered_df['정류소'].unique()
+        selected_station = st.selectbox('검색된 정류장 목록에서 선택하세요:', station_list)
+
+        # 선택된 정류장의 데이터만 필터링
+        selected_data = filtered_df[filtered_df['정류소'] == selected_station]
+
+        st.write(f"### {selected_station}에 대한 정보:")
+        for index, row in selected_data.iterrows():
             st.write(f"**노선**: {row['노선']}")
             st.write(f"**평균 배차 시간**: {row['평균배차시간(분)']}분")
             st.write(f"**첫차 시간**: {row['첫차']}")
