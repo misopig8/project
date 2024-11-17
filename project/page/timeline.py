3a9eba6df34125689a9b8e1c06e50f2043adc6da
misopig8 <misopig8@gmail.com>
2024-11-17 12:28:16 +0900

    Add files via upload

--- project/page/timeline.py
+++ project/page/timeline.py
@@ -25,28 +25,27 @@ if station_name:
 
         st.write(f"### {selected_station}에 대한 정보:")
 
-        # 3개의 열로 나누어 출력
-        cols = st.columns(3)
-        
+        # 3개의 열로 나누어 출력 (시간표 유형 포함)
+        cols = st.columns(3)  # 3개의 열로 유지
+
         for index, row in selected_data.iterrows():
-            # 첫 번째 열에 노선과 배차시간을 함께 출력
+            # 첫 번째 열에 노선과 첫차 출력
             with cols[0]:
                 st.write(f"**노선**: {row['노선']}")
+                st.write(f"**첫차**: {row['첫차']}")
+                st.markdown("<hr>", unsafe_allow_html=True)  # 구분선 추가
+            
+            # 두 번째 열에 막차와 배차시간 출력
+            with cols[1]:
+                st.write(f"**막차**: {row['막차']}")
                 st.write(f"**배차시간**: {row['평균배차시간(분)']}분")
                 st.markdown("<hr>", unsafe_allow_html=True)  # 구분선 추가
-            
-            # 두 번째 열에 첫차와 막차를 함께 출력
-            with cols[1]:
-                st.write(f"**첫차**: {row['첫차']}")
-                st.write(f"**막차**: {row['막차']}")
-                st.markdown("<hr>", unsafe_allow_html=True)  # 구분선 추가
-            
-            # 세 번째 열에 시간표 유형을 출력
+
+            # 세 번째 열에 시간표 유형 출력
             with cols[2]:
                 st.write(f"**시간표 유형**: {row['시간표유형']}")
-                st.markdown("<hr>", unsafe_allow_html=True)
-                st.write("             ")  # 구분선 추가
-            
+                st.markdown("<hr>", unsafe_allow_html=True)  # 구분선 추가
+
             st.write("---")  # 덧붙여서 각 항목을 구분하는 줄 추가
     else:
         st.write(f"'{station_name}'에 해당하는 정류장이 없습니다.")
