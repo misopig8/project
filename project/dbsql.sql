99f409cb198f90341a39b45a7b0b3a93849db54a
misopig8 <misopig8@gmail.com>
2024-11-21 00:53:27 +0900

    Add files via upload

--- project/dbsql.sql
+++ project/dbsql.sql
@@ -3,20 +3,15 @@
 --     userid integer primary key autoincrement, 
 --     username text not null, 
 --     password text not null,
---     email text not null,
---     gender text check(gender in ('male', 'female')),
+--     station_number text not null,
 --     create_at timestamp default create_timestamp
 -- );
---데이터 삽입하기 위한 SQL
---INSERT into user(username,password,email,gender)
---VALUES('test','123','tese@gmail.com'
 
 drop table user 
 create table user(
     userid integer primary key autoincrement, 
     username text not null, 
     password text not null,
-    email text not null,
-    gender text check(gender in ('male', 'female')),
+    station_number text not null,
     create_at timestamp default CURRENT_TIMESTAMP
 );
